be7e82028f5054a3b2dfe34536aee35678826aab
Syed Farhad <139438042+FarhadAgha@users.noreply.github.com>
2026-04-04 08:52:05 +0500

    Add files via upload

---
+++ Inheritance wiht Opps.py
@@ -0,0 +1,35 @@
+#This is the Base/Parent class
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def speak(self):
+        pass  
+    def info(self):
+        return f"{self.name} is {self.age} years old"
+
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} says Woof!"
+    
+    def fetch(self):
+        return f"{self.name} is fetching the ball"
+
+class Cat(Animal):
+    def speak(self):
+        return f"{self.name} says Meow!"
+    
+    def scratch(self):
+        return f"{self.name} is scratching the furniture"
+
+dog = Dog("puppy", 3)
+cat = Cat("kitten", 2)
+
+print(dog.info())        
+print(dog.speak())       
+print(dog.fetch())       
+
+print(cat.info())        
+print(cat.speak())       
+print(cat.scratch())     
